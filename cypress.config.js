e601b71fe422b778dcbc1c479e7cc4a076a0be82
lizarbventurim <lizarbpacheco@gmail.com>
2025-02-10 14:30:53 -0300

    feat: add viewport dimensions for Cypress tests

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,8 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "https://autoforce-academy.pilotodetestes.com.br/ddm-fiat-v2-dois/",
     failOnStatusCode: false,
+    viewportWidth: 1920,
+    viewportHeight: 1080,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
